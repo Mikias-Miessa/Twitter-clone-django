92a84d0b8b49afc4d1a889d09ad77d9492ecafc7
Mikias Miessa <mikiasmiessa@gmail.com>
2023-04-17 23:33:42 +0300

    users, profiles and follows created

--- Twitter/admin.py
+++ Twitter/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group,User
+from .models import Profile
+
+admin.site.unregister(Group)
+
+class ProfileInline(admin.StackedInline):
+    model= Profile
+
+class UserAdmin(admin.ModelAdmin):
+    modal = User
+    
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
+
 
 # Register your models here.
--- Twitter/models.py
+++ Twitter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User , on_delete= models.CASCADE)
+    follows = models.ManyToManyField( "self" ,
+                                     symmetrical=False,
+                                     blank=True,
+                                     related_name="followed_by")
+    def __str__(self):
+        return self.user.username
 # Create your models here.
