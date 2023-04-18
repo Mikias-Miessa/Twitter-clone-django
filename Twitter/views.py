4dfbd2f7b792e526285055fd802696c0ba7a4e65
Mikias Miessa <mikiasmiessa@gmail.com>
2023-04-18 17:45:02 +0300

    users can follow and unfollow each other from now on

--- Twitter/views.py
+++ Twitter/views.py
@@ -10,5 +10,20 @@ def profile_list(request):
         profile= Profile.objects.exclude(user= request.user)
         return render(request, 'profile_list.html', {"profiles" : profile})
     else:
-        messages.success(request,("You must be logged in first ti view profile list"))
+        messages.success(request,("You must be logged in first to view profile list"))
+        return redirect('home')
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id = pk)
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            if action == "follow":
+                current_user_profile.follows.add(profile)
+            else:
+                current_user_profile.follows.remove(profile)
+            current_user_profile.save()    
+        return render(request,'profile.html',{'profile':profile})
+    else:
+        messages.success(request,('You must be logged in first'))
         return redirect('home')
