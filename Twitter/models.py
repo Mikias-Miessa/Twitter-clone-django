c2cff2e7f5f8baa768c94caeb8101fb528b4cd4b
Mikias Miessa <mikiasmiessa@gmail.com>
2023-04-18 11:32:19 +0300

    profiles will be created while user registers

--- Twitter/models.py
+++ Twitter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete= models.CASCADE)
@@ -9,4 +10,12 @@ class Profile(models.Model):
                                      related_name="followed_by")
     def __str__(self):
         return self.user.username
-# Create your models here.
+
+def creat_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user= instance)
+        user_profile.save()
+
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+post_save.connect(creat_profile, sender=User)
